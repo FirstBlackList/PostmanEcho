7694694206324c395f2ca56abce4f8e762bd95dc
BlackList <azokblack@gmail.com>
2023-03-29 21:40:17 +0400

    Initial commit and FirstBuild

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldPostmanEchoAnswer() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Передача данных")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Передача данных"))
+        ;
+    }
+}
